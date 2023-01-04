25a7fbb2cc8e8e20b181f8dcd78930a47e37cf4b
kavya <kavyareddy41997@gmail.com>
2023-01-03 17:07:18 -0700

    updated

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    
+    const queue = new Queue(this, 'HelloCdkQueue',{
+        visibilityTimeout: cdk.Duration.seconds(300)
+    });
+      
+     const newBucket = new Bucket(this, 'MyFirstBucket',{
+         versioned: true,
+         removalPolicy: cdk.RemovalPolicy.DESTROY,
+         autoDeleteObjects: true
+     });
+         
   }
 }
